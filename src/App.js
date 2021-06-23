10781b49b15f0ad2ca9d0afddbb5ceb6b5c783e8
Dzmitry Davidovich <Dzmitrydavidovich89@gmail.com>
2021-06-24 02:33:27 +0300

    add cart layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,42 @@ import React from "react";
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина
+          <img className="remove-btn" src="/img/btn-remove.svg" alt="Remove" />
+
+          </h2>
+          <div className="cart-items">
+            <div className="cart-item">
+              <div className="cart-img" style= {{backgroundImage: 'url(/img/sneakers/1.jpg)'}}></div>
+              <div className="mr-10">
+                <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <b>12 999</b>
+              </div>
+              <img className="remove-btn" src="/img/btn-remove.svg" alt="Remove" />
+            </div>
+          </div>
+
+          <div className="cart-total">
+            <ul className="cart-summary" >
+              <li className="cart-summary__item">
+                <span>Итого:</span>
+                <div></div>
+                <b>21 648</b>
+              </li>
+              <li className="cart-summary__item">
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074</b>
+              </li>
+            </ul>
+            <button className="greenBtn">Оформить заказ
+              <img  src="/img/Arrow.svg" alt="ArrowRight" />
+            </button>
+          </div>
+        </div>
+      </div>
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img className="mr-20" src="img/sneakersLogo.png" alt="logo" />
@@ -21,14 +57,26 @@ function App() {
               </li>
           </ul>
       </header>
-      <div className="content p-40">
+      <div className="content">
+        <div className="d-flex justify-between align-center mb-40px">
+
         <h1 >Все кроссовки</h1>
+        <div className="search-block">
+          <img width={20} height={20} src="/img/Search.svg" alt="search" />
+            <input
+              placeholder="Search ..."
+              className="search-input"
+              type="text" />
+
+        </div>
+        </div>
         <div className="sneakers">
           <div className="card">
-            <div className="d-flex  flex-column">
+            <div className="favorite">
+            <img src="/img/HeartUnliked.svg" alt="heart" /> 
+            </div>
             <img height={112} width={132} src="/img/sneakers1.jpg" alt="sneakers"/>
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            </div>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
                 <span>Цена: </span>
@@ -39,6 +87,7 @@ function App() {
               </button>
             </div>
           </div>
+
           <div className="card">
             <div className="d-flex  flex-column">
             <img height={112} width={132} src="/img/sneakers1.jpg" alt="sneakers"/>
